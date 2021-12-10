56e527581b4387cb5f9bc51eec5f6408d9d069ed
mcl19909949541 <1693738649@qq.com>
2021-12-10 17:06:57 +0800

    修改主菜单

---
+++ client/src/main/java/org/example/client/funtion/Data.java
@@ -0,0 +1,11 @@
+package org.example.client.funtion;
+
+import javax.swing.*;
+import java.net.URL;
+
+public class Data {
+
+    public static URL backgroundURL = Data.class.getResource("src/main/java/org/example/client/images/background.jpg");
+    public static ImageIcon background = new ImageIcon(backgroundURL);
+
+}
