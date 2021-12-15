d7d1caa1590ac060a148a3b4c6958a5361fa9850
廖菁璞 <liaojingpu@126.com>
2021-12-15 15:05:50 +0800

    feat : 服务器和客户端的地图统一用Level

--- common/src/main/java/org/example/common/config/level/Level1.java
+++ common/src/main/java/org/example/common/config/level/Level1.java
@@ -1,21 +1,20 @@
 package org.example.common.config.level;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 import org.example.common.config.GameConfig;
 import org.example.common.model.monster.Monster;
 import org.example.common.model.monster.MonsterFactory;
 
-@NoArgsConstructor
-public class Level1 {
-    public static final int NUMBER = 1;
-    public static final int SIDE_LENGTH = 1000;
-    public static final boolean[][] TERRAIN = new boolean[SIDE_LENGTH][SIDE_LENGTH];
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Level1 extends Level {
 
-    public static final Monster[] INITIAL_MONSTERS={
-            MonsterFactory.makeMonster(GameConfig.MonsterType.ONE,"m1",300,300,90.0),
-            MonsterFactory.makeMonster(GameConfig.MonsterType.ONE, "m2", 500, 500, 180.0),
-            MonsterFactory.makeMonster(GameConfig.MonsterType.ONE,"m3",500,500,270.0)
-    };
+    public Level1() {
+        super(1, 1000, new boolean[1000][1000], new Monster[]{
+                MonsterFactory.makeMonster(GameConfig.MonsterType.ONE, "m1", 300, 300, 90.0),
+                MonsterFactory.makeMonster(GameConfig.MonsterType.ONE, "m2", 500, 500, 180.0),
+                MonsterFactory.makeMonster(GameConfig.MonsterType.ONE, "m3", 500, 500, 270.0)
+        });
+    }
 }
