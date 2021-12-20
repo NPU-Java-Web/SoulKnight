8e65a4cdf5d23e0c2cef0b7dc03cc50628f10e06
mcl19909949541 <1693738649@qq.com>
2021-12-20 16:33:41 +0800

    修改人物属性

--- common/src/main/java/org/example/common/model/bullet/entity/Bullet2.java
+++ common/src/main/java/org/example/common/model/bullet/entity/Bullet2.java
@@ -7,7 +7,7 @@ public class Bullet2 extends Bullet {
     public static final int BULLET_TYPE= GameConfig.BulletType.ARMOR;
     public static final int SPEED = 30;
     public static final int RADIUS = 40;
-    public static final int POWER = 5;
+    public static final int POWER = 10;
 
     public Bullet2(String playerId, Integer x, Integer y, Double angle){
         super(BULLET_TYPE, null, playerId, x, y, angle, SPEED, RADIUS, POWER,System.currentTimeMillis());
