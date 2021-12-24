a55a5c7c831aa2628c91ec0d605cecf274ab250a
86183 <1979557180@qq.com>
2021-12-24 11:25:26 +0800

    fix: fix shoot bug of Monster5

--- common/src/main/java/org/example/common/model/monster/entity/Monster5.java
+++ common/src/main/java/org/example/common/model/monster/entity/Monster5.java
@@ -13,7 +13,7 @@ public class Monster5 extends Monster {
     public static final int MONSTER_TYPE = 5;
     public static final int SPEED = 3;
     public static final int BLOOD = 100;
-    public static final int CD = 200;
+    public static final int CD = 175;
     public static final int VISIBILITY = 400;
     public static final int REWARD = 50;
     private static int count;
@@ -27,8 +27,6 @@ public class Monster5 extends Monster {
         count = count % 360;
         List<Bullet> bullets = new ArrayList<>();
         bullets.add(BulletFactory.makeBullet(GameConfig.BulletType.LIGHT_BLUE, monster.getMonsterId(), monster.getX(), monster.getY(), Math.toRadians(count)));
-        bullets.add(BulletFactory.makeBullet(GameConfig.BulletType.LIGHT_BLUE, monster.getMonsterId(), monster.getX(), monster.getY(), Math.toRadians(count + 120)));
-        bullets.add(BulletFactory.makeBullet(GameConfig.BulletType.LIGHT_BLUE, monster.getMonsterId(), monster.getX(), monster.getY(), Math.toRadians(count + 240)));
         count += 20;
         return bullets;
     }
