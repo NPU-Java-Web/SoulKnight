f089af19f30798bd02a0aa94974d49eae6b63521
86183 <1979557180@qq.com>
2021-12-25 15:54:56 +0800

    feature:change the place of monster

--- client/src/test/java/org/example/client/ClientApplicationTest.java
+++ client/src/test/java/org/example/client/ClientApplicationTest.java
@@ -1,5 +1,8 @@
 package org.example.client;
 
+import org.example.common.config.level.Level;
+import org.example.common.config.level.Level1;
+import org.example.common.config.level.Level2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +12,14 @@ public class ClientApplicationTest {
         int num1 = 1 + 2 + 3 + 4;
         int num2 = 10;
         Assertions.assertEquals(num1, num2);
-//        System.out.println(getClass().getResource());
+        System.out.println(getClass());
+    }
+
+    @Test
+    public void testPoint(){
+        int x = 750;
+        int y = 600;
+        Level level = new Level2();
+        System.out.println(level.getTerrain()[x][y]);
     }
 }
